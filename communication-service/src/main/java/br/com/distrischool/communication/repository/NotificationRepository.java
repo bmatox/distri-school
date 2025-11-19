8f2f8eacdc7c3791ddbbe07ef6eff01b288685e9
51644 <51644@tjce.jus.br>
2025-11-19 11:00:00 -0300

    feat(communication-service): adicionar interface NotificationRepository

---
+++ communication-service/src/main/java/br/com/distrischool/communication/repository/NotificationRepository.java
@@ -0,0 +1,17 @@
+package br.com.distrischool.communication.repository;
+
+import br.com.distrischool.communication.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    
+    List<Notification> findByUserId(Long userId);
+    
+    List<Notification> findByUserIdAndIsRead(Long userId, Boolean isRead);
+    
+    List<Notification> findByNotificationType(String notificationType);
+}
