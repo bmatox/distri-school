6c38b57957e19b054702009287738f1aab1d63b7
51644 <51644@tjce.jus.br>
2025-11-19 15:00:00 -0300

    feat(communication-service): adicionar DTO CreateNotificationRequest

---
+++ communication-service/src/main/java/br/com/distrischool/communication/dto/CreateNotificationRequest.java
@@ -0,0 +1,25 @@
+package br.com.distrischool.communication.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateNotificationRequest {
+
+    @NotNull(message = "User ID is required")
+    private Long userId;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Message is required")
+    private String message;
+
+    @NotBlank(message = "Notification type is required")
+    private String notificationType;
+}
