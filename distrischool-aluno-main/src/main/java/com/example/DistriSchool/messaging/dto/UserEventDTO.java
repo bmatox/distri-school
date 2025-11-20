c77861431b9f8002bf75e9916c6251524cf566af
51644 <51644@tjce.jus.br>
2025-11-20 18:00:00 -0300

    feat(aluno-service): adicionar DTOs de eventos para integração

---
+++ distrischool-aluno-main/src/main/java/com/example/DistriSchool/messaging/dto/UserEventDTO.java
@@ -0,0 +1,20 @@
+package com.example.DistriSchool.messaging.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEventDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+    private Long externalId;
+    private String userType;
+    private String type; // CREATED, UPDATED, DELETED
+}
