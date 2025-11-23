f8f5a178148c10e4e2e5579d02b89ae704799cc2
51644 <51644@tjce.jus.br>
2025-11-23 12:00:00 -0300

    Adiciona classe principal do serviço de notas

---
+++ grades-service/src/main/java/br/com/distrischool/grades/GradesServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.distrischool.grades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradesServiceApplication.class, args);
+    }
+}
