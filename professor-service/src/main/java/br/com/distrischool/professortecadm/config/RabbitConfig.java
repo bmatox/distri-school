3b91e0ecf9f1f1b5751fc089af2f1a0eccc9e8ee
51644 <51644@tjce.jus.br>
2025-11-24 12:00:00 -0300

    Adiciona configurações iniciais do serviço de professor

---
+++ professor-service/src/main/java/br/com/distrischool/professortecadm/config/RabbitConfig.java
@@ -0,0 +1,56 @@
+package br.com.distrischool.professortecadm.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String EXCHANGE = "distrischool.events.exchange";
+    public static final String PROFESSOR_EVENTS_QUEUE = "professor-service.professor-events";
+    public static final String PROFESSOR_ROUTING_KEY = "professor.*";
+    public static final String TECNICOADMIN_EVENTS_QUEUE = "professor-service.tecnicoadmin-events";
+    public static final String TECNICOADMIN_ROUTING_KEY = "tecnicoadmin.*";
+
+    @Bean
+    public TopicExchange distrischoolExchange() {
+        return ExchangeBuilder.topicExchange(EXCHANGE).durable(true).build();
+    }
+
+    @Bean
+    public Queue professorEventsQueue() {
+        return QueueBuilder.durable(PROFESSOR_EVENTS_QUEUE).build();
+    }
+
+    @Bean
+    public Queue tecnicoAdminEventsQueue() {
+        return QueueBuilder.durable(TECNICOADMIN_EVENTS_QUEUE).build();
+    }
+
+    @Bean
+    public Binding professorEventsBinding(Queue professorEventsQueue, TopicExchange distrischoolExchange) {
+        return BindingBuilder.bind(professorEventsQueue).to(distrischoolExchange).with(PROFESSOR_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding tecnicoAdminEventsBinding(Queue tecnicoAdminEventsQueue, TopicExchange distrischoolExchange) {
+        return BindingBuilder.bind(tecnicoAdminEventsQueue).to(distrischoolExchange).with(TECNICOADMIN_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+}
