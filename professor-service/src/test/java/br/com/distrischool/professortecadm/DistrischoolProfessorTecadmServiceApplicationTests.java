00c88130a33154b5244a87a2f9f90250f47956b1
51644 <51644@tjce.jus.br>
2025-12-06 02:32:31 -0300

    Adiciona arquivos de testes do modulo prof

---
+++ professor-service/src/test/java/br/com/distrischool/professortecadm/DistrischoolProfessorTecadmServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package br.com.distrischool.professortecadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DistrischoolProfessorTecadmServiceApplicationTests {
+
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+}
