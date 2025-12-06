439c9da60d6f9c8acbaa0a4ca2882c30ca529dd0
51644 <51644@tjce.jus.br>
2025-12-06 02:30:30 -0300

    define DTOs de entrada e saída para endpoints da api...

---
+++ professor-service/src/main/java/br/com/distrischool/professortecadm/dto/ProfessorResponse.java
@@ -0,0 +1,13 @@
+package br.com.distrischool.professortecadm.dto;
+
+import java.time.LocalDate;
+
+public record ProfessorResponse(
+        Long id,
+        String nome,
+        String email,
+        String especialidade,
+        LocalDate dataContratacao,
+        Long userId,
+        Long turmaId
+) {}
