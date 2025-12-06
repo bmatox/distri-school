7c7c3897dc9921517d2d40db33dee5d368268c82
51644 <51644@tjce.jus.br>
2025-12-06 02:31:10 -0300

    feat adiciona exceções personalizadas para regras de negócio

---
+++ professor-service/src/main/java/br/com/distrischool/professortecadm/exception/EspecialidadeMismatchException.java
@@ -0,0 +1,7 @@
+package br.com.distrischool.professortecadm.exception;
+
+public class EspecialidadeMismatchException extends RuntimeException {
+    public EspecialidadeMismatchException(String message) {
+        super(message);
+    }
+}
